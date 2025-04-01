9d47185c4c1fc4864f1e5970e1481a0847910271
Andrés Steinshouer <Ramirez.cr10@hotmail.com>
2025-04-01 04:04:19 -0600

    correcciones citavm

--- app/src/main/java/com/example/agendamedica/viewmodel/CitaViewModel.kt
+++ app/src/main/java/com/example/agendamedica/viewmodel/CitaViewModel.kt
@@ -1,18 +1,54 @@
 package com.example.agendamedica.viewmodel
 
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.agendamedica.model.CitaModel
+import com.example.agendamedica.model.Ubicacion
+import com.example.agendamedica.ui.state.CitaState
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import com.example.agendamedica.ui.state.CitaState
-import com.example.agendamedica.model.CitaModel
 
-class CitaViewModel {
+
+class CitaViewModel  : ViewModel() {
 
     private val _state = mutableStateOf(CitaState())
     val state: State<CitaState> = _state
+
+    // Simulación de base de datos local (mientras no conectes Firebase)
+    private val citasSimuladas = mutableListOf<CitaModel>()
+
+    // Función para agregar una cita
+    fun agregarCita(
+        fecha: String,
+        hora: String,
+        provincia: String,
+        hospital: String,
+        medico: String,
+        motivo: String
+    ) {
+        viewModelScope.launch {
+            _state.value = _state.value.copy(isLoading = true)
+
+            val nuevaCita = CitaModel(
+                idCita = System.currentTimeMillis().toString(),
+                fecha = fecha,
+                hora = hora,
+                ubicacion = Ubicacion(provincia, hospital),
+                medico = medico,
+                motivo = motivo,
+                estado = "confirmada"
+            )
+
+            citasSimuladas.add(nuevaCita)
+
+            _state.value = _state.value.copy(
+                isLoading = false,
+                citas = citasSimuladas.toList()
+            )
+        }
+    }
 
     fun cargarCitas() {
         _state.value = _state.value.copy(isLoading = true)
