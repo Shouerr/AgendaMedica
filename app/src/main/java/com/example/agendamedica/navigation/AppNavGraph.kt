f015103b0f22119d19f9f4aeb65afc066b98b470
Andrés Steinshouer <Ramirez.cr10@hotmail.com>
2025-04-07 01:30:30 -0600

    AppNav arreglado

--- app/src/main/java/com/example/agendamedica/navigation/AppNavGraph.kt
+++ app/src/main/java/com/example/agendamedica/navigation/AppNavGraph.kt
@@ -1,19 +1,39 @@
 package com.example.agendamedica.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.agendamedica.ui.screens.* // trae todo de la carpeta de screens
+import com.example.agendamedica.ui.screens.CitaScreen
+import com.example.agendamedica.ui.screens.HomeScreen
+import com.example.agendamedica.ui.screens.LoginScreen
+import com.example.agendamedica.ui.screens.PerfilScreen
+import com.example.agendamedica.ui.screens.RegistroScreen
+import com.example.agendamedica.viewmodel.AuthViewModel
 
 
 @Composable
-fun AppNavGraph(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = "login") {
+fun AppNavGraph(navController: NavHostController, authViewModel: AuthViewModel = viewModel()) {
+    val userState by authViewModel.user.collectAsState()
+
+    // Si el estado del usuario cambia, manejamos la navegación
+    if (userState != null) {
+        LaunchedEffect(userState) {
+            navController.navigate("home") {
+                popUpTo("login") { inclusive = true }
+            }
+        }
+    }
+
+    NavHost(navController = navController, startDestination = if (userState != null) "home" else "login") {
         composable("login") { LoginScreen(navController) }
         composable("registro") { RegistroScreen(navController) }
         composable("home") { HomeScreen(navController) }
-        composable("cita") { CitaScreen(navController) }
         composable("perfil") { PerfilScreen(navController) }
+        composable("cita") { CitaScreen(navController) }
     }
 }
